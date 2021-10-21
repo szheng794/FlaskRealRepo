9fb1f420170514b4e30046b0e8e7c2671a507a75
Stone Zheng <szheng2@uncc.edu>
2021-10-21 11:56:57 -0400

    flask app #2 - created a route and view to display all notes

--- flask02.py
+++ flask02.py
@@ -12,12 +12,16 @@ app = Flask(__name__)     # create an app
 
 @app.route('/')
 @app.route('/index')
+@app.route('/notes')
+#def index():
+   # a_user= {'name':'Stone','email': 'szheng2@@uncc.edu'}
+    #return render_template('index.html', user = a_user)
 
+def get_notes():
+    notes = {1: {'title: ' : 'First note' , 'text' : 'This is my first note' , 'date' : '10-1-2020'},
+             2: {'title' : 'Second note' , 'text' : 'This is my second note', ' date' : '10-2-2020'} }
 
-def index():
-    a_user= {'name':'Stone','email': 'szheng2@@uncc.edu'}
-    return render_template('index.html', user = a_user)
-
+    return render_template('notes.html' , notes = notes)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
