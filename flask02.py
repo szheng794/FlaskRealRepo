e7061155afda11f4e814a0d3d7f2b89b871afc6f
Stone Zheng <szheng2@uncc.edu>
2021-10-25 10:28:56 -0400

    flask app #2 - created a dynamic welcome route

--- flask02.py
+++ flask02.py
@@ -1,25 +1,27 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 from flask import render_template
 # imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask   # Flask is the web app that we will customize
+import os  # os is used to get environment variables IP & PORT
+from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
 from flask import Flask
 
 from flask import Flask
-app = Flask(__name__, template_folder='template')
-@app.route("/")# create an app
+
+app = Flask(__name__)
+
+
+@app.route("/")  # create an app
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/index')
-
 def index():
-    a_user = {'name' : 'Stone' , 'email' : 'szheng2@uncc.edu'}
-    return render_template("index.html")
+    a_user = {'name': 'Stone', 'email': 'szheng2@uncc.edu'}
+    return render_template("index.html", user=a_user)
 
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
 # To see the web page in your web browser, go to the url,
 #   http://127.0.0.1:5000
