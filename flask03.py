efda80895050ca127efdcb7de39e3b628d21ec52
Stone Zheng <szheng2@uncc.edu>
2021-10-26 09:27:46 -0400

    flask app #3 - created initial app file

--- flask03.py
+++ flask03.py
@@ -1,5 +1,5 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
-from flask import render_template
+from flask import render_template, url_for
 # imports
 import os  # os is used to get environment variables IP & PORT
 from flask import Flask  # Flask is the web app that we will customize
@@ -7,8 +7,15 @@ from flask import render_template
 from flask import Flask
 from flask import request
 from flask import Flask
+from werkzeug.utils import redirect
+from flask import redirect, url_for
 
 app = Flask(__name__)
+notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10/25/2021'},
+         2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10/25/2021'},
+         3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10/25/2021'},
+         4: {'title': 'Fourth note', 'text': 'This is my fourth note', 'date': '10/25/2021'}
+         }
 
 
 @app.route("/")  # create an app
@@ -22,34 +29,38 @@ def index():
     return render_template("index.html", user=a_user)
 
 
-@app.route('/')
 @app.route('/notes')
 def get_notes():
     notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10/25/2021'},
              2: {'title': 'Second note', 'text': 'This is my first note', 'date': '10/25/2021'},
-             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10/25/2021'}}
+             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10/25/2021'},
+             4: {'title': 'Fourth note', 'text': 'This is my fourth note', 'date': '10/25/2021'}
+             }
 
     return render_template('notes.html', notes=notes)
 
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10/25/2021'},
-             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10/25/2021'},
-             3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10/25/2021'}
-             }
     return render_template('note.html', note=notes[int(note_id)])
 
+
 @app.route('/notes/new', methods=['GET', 'POST'])
-def new_note():
+def new_note(user=None):
     a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
     print('request method is', request.method)
     if request.method == 'POST':
-
-        return '<h1> POST method used for this request </h1>'
+        title = request.form['title']
+        text = request.form['noteText']
+        from datetime import date
+        today = date.today()
+        today = today.strftime("%m-%d-%Y")
+        id = len(notes) + 1
+        request_data = request.form
+        notes[id] = {'title': title, 'text': text, 'date': today, }
+        return redirect(url_for('get_notes', name=user))
     else:
-        return render_template('new.html', user = a_user)
-
+        return render_template('new.html', user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
