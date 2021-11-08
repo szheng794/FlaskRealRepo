f2c8ecf76b76d71d7a6beb38e5ae4907e964968e
Stone Zheng <szheng2@uncc.edu>
2021-11-08 10:55:55 -0500

    flask app #5 - edit note function hadnles post and get

--- flask05.py
+++ flask05.py
@@ -72,12 +72,32 @@ def new_note():
         a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
         return render_template('new.html', user=a_user)
 
-@app.route('/notes/edit/<note_id>')
+@app.route('/notes/edit/<note_id>', methods = ['GET' , 'POST'])
 def update_note(note_id):
-    a_user = db.session.query(User).filter_by(email= 'mogli@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id = note_id).one()
+      if request.method == 'POST':
 
-    return render_template('new.html', note = my_note, user = a_user)
+        title = request.form['title']
+
+        text = request.form['noteText']
+
+        note = db.session.query(Note).filter_by(id = note_id).one()
+
+        note.title = title
+
+        note.text = text
+
+        db.session.add(note)
+
+        db.session.commit()
+
+
+        a_user = db.session.query(User).filter_by(email= 'mogli@uncc.edu').one()
+
+        my_note = db.session.query(Note).filter_by(id = note_id).one()
+
+        return render_template('new.html', note = my_note, user = a_user)
+
+
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
